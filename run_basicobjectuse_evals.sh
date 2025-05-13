6b92198270bf1523bba9a5c69903deaef28f93e4
daphnedemekas <daphnedemekas@gmail.com>
2025-05-13 16:38:58 -0700

    object use

--- run_basicobjectuse_evals.sh
+++ run_basicobjectuse_evals.sh
@@ -2,12 +2,10 @@
 
 # Define the list of policy URIs
 POLICIES=(
-    "b.daveey.t.8.rdr9.3"
-    "b.daveey.t.4.rdr9.3"
-    "b.daveey.t.8.rdr9.mb2.1"
-    "daveey.t.1.pi.dpm"
-    "b.daveey.t.64.dr90.1"
-    "b.daveey.t.8.rdr9.sb"
+    "daphne.3object_use_colors"
+    "daphne.3object_use_no_colors"
+    "dd_object_use_easy2"
+    "daphne.2object_use_colors_pretrained"
 )
 
 
@@ -20,5 +18,9 @@ for i in "${!POLICIES[@]}"; do
         eval=object_use \
         run=george_object_use_basic_evaluation$IDX \
         eval.policy_uri=wandb://run/$POLICY_URI \
-        eval_db_uri=wandb://artifacts/object_use_db
+        eval_db_uri=wandb://artifacts/object_use \
+
+
+python3 -m tools.analyze +eval_db_uri=wandb://artifacts/object_use run=object_use_db ++analyze.output_path=s3://softmax-public/policydash/objectuse.html \
+
 done
