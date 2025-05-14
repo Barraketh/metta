3b259f8948d17163fdcf575784df5c1af619f967
daphnedemekas <daphnedemekas@gmail.com>
2025-05-13 17:43:53 -0700

    update script

--- run_basicobjectuse_evals.sh
+++ run_basicobjectuse_evals.sh
@@ -16,10 +16,10 @@ for i in "${!POLICIES[@]}"; do
     IDX="${IDX}_${RANDOM_NUM}"
     echo "Running full sequence eval for policy $POLICY_URI"
     python3 -m tools.sim \
-        eval=object_use \
-        run=george_object_use_basic_evaluation$IDX \
-        eval.policy_uri=wandb://run/$POLICY_URI \
-        eval_db_uri=wandb://artifacts/object_use \
+        sim=object_use \
+        run=object_use$IDX \
+        policy_uri=wandb://run/$POLICY_URI \
+        +eval_db_uri=wandb://artifacts/object_use \
 
 
 python3 -m tools.analyze +eval_db_uri=wandb://artifacts/object_use run=object_use_db ++analyze.output_path=s3://softmax-public/policydash/objectuse.html \
